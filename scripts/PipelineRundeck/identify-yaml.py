c7d181e04874bbacbc435dacfc0b0599b5a2af2d
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2023-11-23 10:14:19 -0300

    Update identify-yaml.py

--- scripts/PipelineRundeck/identify-yaml.py
+++ scripts/PipelineRundeck/identify-yaml.py
@@ -1,35 +1,41 @@
 import requests
 import os
 
-def get_modified_yaml_files(username, repo_name, token):
-    url = f'https://api.github.com/repos/{username}/{repo_name}/commits'
+def get_modified_yaml_files(repo_name, token):
+    url = f'https://api.github.com/repos/{repo_name}/commits'
     headers = {'Authorization': f'token {token}'}
 
-    # Obtém o último commit
+    # Obtém a lista de commits
     response = requests.get(url, headers=headers)
-    commit = response.json()[0]  # Assume que o primeiro item é o último commit
 
-    # Obtém a lista de arquivos modificados no último commit
-    commit_sha = commit['sha']
-    commit_url = f'https://api.github.com/repos/{username}/{repo_name}/commits/{commit_sha}'
-    commit_response = requests.get(commit_url, headers=headers)
-    files = commit_response.json()['files']
+    # Verifica se há pelo menos um commit
+    if response.status_code == 200 and response.json():
+        # Assume que o primeiro item é o último commit
+        commit = response.json()[0]
 
-    # Filtra os arquivos YAML, excluindo os pipelines na pasta .github/workflows
-    yaml_files = [file['filename'] for file in files if file['filename'].lower().endswith(('.yaml', '.yml')) and '.github/workflows' not in file['filename']]
+        # Obtém a lista de arquivos modificados no último commit
+        commit_sha = commit['sha']
+        commit_url = f'https://api.github.com/repos/{repo_name}/commits/{commit_sha}'
+        commit_response = requests.get(commit_url, headers=headers)
+        files = commit_response.json()['files']
 
-    # Retorna a lista separada por vírgulas
-    if yaml_files:
-        return ','.join(yaml_files)
+        # Filtra os arquivos YAML, excluindo os pipelines na pasta .github/workflows
+        yaml_files = [file['filename'] for file in files if file['filename'].lower().endswith(('.yaml', '.yml')) and '.github/workflows' not in file['filename']]
+
+        # Retorna a lista separada por vírgulas
+        if yaml_files:
+            return ','.join(yaml_files)
+        else:
+            return None
     else:
+        # Sem commits encontrados
         return None
 
-# Substitua 'LeoBGs1', 'app-cinema' e os.getenv('GH_TOKEN') pelos seus valores reais
-username = 'Ll2223'
+# Substitua 'app-cinema' e os.getenv('GH_TOKEN') pelos seus valores reais
 repo_name = os.environ.get('GITHUB_REPOSITORY')
 token = os.getenv('GH_TOKEN')
 
-result = get_modified_yaml_files(username, repo_name, token)
+result = get_modified_yaml_files(repo_name, token)
 
 if result is not None:
     print(result)
