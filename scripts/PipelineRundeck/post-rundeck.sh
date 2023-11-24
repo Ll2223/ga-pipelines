94457dd1719b539ca74f3372bb332bf57fc3447e
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2023-11-24 19:20:18 -0300

    Update post-rundeck.sh

--- scripts/PipelineRundeck/post-rundeck.sh
+++ scripts/PipelineRundeck/post-rundeck.sh
@@ -12,8 +12,9 @@ rdeck_api="38"
 rdeck_project="${ENV}"  # Utiliza a variável de ambiente ENV definida no GitHub Actions
 
 # Obtendo a lista de arquivos YAML do script Python
-yaml_files_csv=$(python identify-yaml.py)  # Substitua por como você obtém a lista no seu script Python
-IFS=',' read -ra yaml_files <<< "$yaml_files_csv"
+IFS=',' read -ra yaml_files << EOF
+$yaml_files_csv
+EOF
 
 # Iterando sobre a lista de arquivos e fazendo a chamada de API para cada um
 for yaml_file in "${yaml_files[@]}"; do
