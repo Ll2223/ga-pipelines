fbc8471d2ef12bd53a6f1b6524daba676b6c277e
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2023-11-24 14:18:00 -0300

    Create post-rundeck.sh

---
+++ scripts/PipelineRundeck/post-rundeck.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# protocol
+protocol="https"
+
+# basic rundeck info
+rdeck_host="host"
+rdeck_port="port"
+rdeck_api="38"
+
+# specific api call info
+rdeck_project="${ENV}"  # Utiliza a variável de ambiente ENV definida no GitHub Actions
+
+# Obtendo a lista de arquivos YAML do script Python
+yaml_files_csv=$(python trasncricao.py)  # Substitua por como você obtém a lista no seu script Python
+IFS=',' read -ra yaml_files <<< "$yaml_files_csv"
+
+# Iterando sobre a lista de arquivos e fazendo a chamada de API para cada um
+for yaml_file in "${yaml_files[@]}"; do
+  # api call
+  curl -kSsv --header "X-Rundeck-Auth-Token:${RUNDECK_TOKEN}" \
+   -F xmlBatch=@"$yaml_file" \
+   "$protocol://$rdeck_host:$rdeck_port/api/$rdeck_api/project/$rdeck_project/jobs/import?fileformat=yaml"
+done
