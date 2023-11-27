be83c0d26100fe70954b4ff430e3ca402e864f19
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2023-11-27 11:57:25 -0300

    Update post-rundeck.sh

--- scripts/PipelineRundeck/post-rundeck.sh
+++ scripts/PipelineRundeck/post-rundeck.sh
@@ -16,25 +16,16 @@ rdeck_api="45"
 # specific api call info
 rdeck_project="${ENV}"  # Utiliza a variável de ambiente ENV definida no GitHub Actions
 
-# Obtém os arquivos modificados diretamente do GitHub Actions
-IFS=',' read -ra modified_files <<< "$MODIFIED_FILES"
+# Obtém os caminhos dos arquivos modificados
+IFS=$'\n' read -d '' -ra modified_files < "$MODIFIED_FILES_PATH"
 
 # Verifica se há arquivos YAML modificados
 if [ "${#modified_files[@]}" -gt 0 ]; then
   for yaml_file in "${modified_files[@]}"; do
-    # Obtém o diretório do arquivo original
-    dir=$(dirname "$yaml_file")
-
-    # Cria o arquivo temporário no mesmo diretório do arquivo original
-    temp_file="$dir/$(basename "$yaml_file").tmp"
-
     # api call
     curl -kSsv --header "X-Rundeck-Auth-Token:${RUNDECK_TOKEN}" \
       -F "xmlBatch=@$yaml_file" \
       "$protocol://$rdeck_host:$rdeck_port/api/$rdeck_api/project/$rdeck_project/jobs/import?fileformat=yaml"
-
-    # Move o arquivo temporário para o arquivo original
-    mv "$yaml_file.tmp" "$yaml_file"
   done
 else
   echo "Nenhum arquivo YAML modificado encontrado após envsubst."
