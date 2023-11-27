6090b366fd3991c19f945440f2fce4335e25d63f
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2023-11-27 11:11:38 -0300

    Update post-rundeck.sh

--- scripts/PipelineRundeck/post-rundeck.sh
+++ scripts/PipelineRundeck/post-rundeck.sh
@@ -13,15 +13,12 @@ rdeck_api="45"
 # specific api call info
 rdeck_project="${ENV}"  # Utiliza a variável de ambiente ENV definida no GitHub Actions
 
-# Substituir variáveis nos arquivos YAML e obter a lista de arquivos modificados
-for yaml_file in $(find . -type f \( -name '*.yml' -o -name '*.yaml' \) -not -path '*.github/workflows*' -print); do
-  envsubst < "$yaml_file" > "$yaml_file.tmp" && mv "$yaml_file.tmp" "$yaml_file"
-  yaml_files+=("$yaml_file")
-done
+# Obtém os arquivos modificados do passo anterior
+IFS=',' read -ra modified_files <<< "${{ steps.replace-with-envsubst.outputs.modified_files }}"
 
 # Verifica se há arquivos YAML modificados
-if [ "${#yaml_files[@]}" -gt 0 ]; then
-  for yaml_file in "${yaml_files[@]}"; do
+if [ "${#modified_files[@]}" -gt 0 ]; then
+  for yaml_file in "${modified_files[@]}"; do
     # api call
     curl -kSsv --header "X-Rundeck-Auth-Token:${RUNDECK_TOKEN}" \
       -F xmlBatch=@"$(pwd)/$yaml_file" \
